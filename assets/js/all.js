393b37d2a5eaddf470a4fb3467cce04424583105
Ho <edc2468925@gmail.com>
2021-08-30 02:32:48 +0800

    Update 2021-08-29T18:32:46.304Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -6,4 +6,47 @@ var swiper = new Swiper(".mySwiper", {
     dynamicBullets: true
   }
 });
+var swiper = new Swiper(".mySwiper1", {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  }
+});
+var swiper = new Swiper(".teacher-card", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  }
+});
+var swiper = new Swiper(".img-card", {
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  breakpoints: {
+    313: {
+      slidesPerView: 2,
+      spaceBetween: 10
+    },
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 3.5,
+      spaceBetween: 20
+    },
+    // when window width is >= 640px
+    992: {
+      slidesPerView: 4.5,
+      spaceBetween: 30
+    }
+  }
+});
+var elem = document.querySelector('input[name="datepicker"]');
+var datepicker = new Datepicker(elem, {});
 //# sourceMappingURL=all.js.map
